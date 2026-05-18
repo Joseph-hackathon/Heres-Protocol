c28d3636f6ba0a54bd78550b2e982af80ed20f88
okelo0121 <angeloulak2004@gmail.com>
2026-05-18 12:54:38 +0300

    metadata title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Heres - Privacy-Preserving Capsule Protocol on Solana',
+  title: 'Heres - Privacy-Preserving Death Insurance Protocol on Solana',
   description:
     'A privacy-preserving capsule protocol on Solana. Assets stay delegated, conditions stay private inside Magicblock ER, execution happens automatically when silence becomes truth. Powered by Helius & Phantom.',
   manifest: '/manifest.json',
